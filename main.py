67fe1ae5bc9f6d858703ccd12d9b121ffce87a27
melisk12 <karci.meliss@gmail.com>
2024-01-21 14:50:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,384 @@
+#####################################################
+# Demand Forecasting
+#####################################################
+
+# Store Item Demand Forecasting Challenge
+# https://www.kaggle.com/c/demand-forecasting-kernels-only
+!pip install lightgbm
+#conda install lightgbm
+
+import time
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+import warnings
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+warnings.filterwarnings('ignore')
+
+def check_data(dataframe):
+    """
+    Display key information about a DataFrame, including shape, head, tail, missing values, and descriptive statistics.
+
+    Parameters:
+    - dataframe (pd.DataFrame): The DataFrame to be checked.
+
+    Returns:
+    This function prints the following information:
+    1. Information: General information about the DataFrame.
+    2. Data Shape: Shape of the DataFrame (number of rows and columns).
+    3. The First 5 Data: Display the first 5 rows of the DataFrame.
+    4. The Last 5 Data: Display the last 5 rows of the DataFrame.
+    5. Missing Values: Display the count of missing values in each column.
+    6. Describe the Data: Display basic statistical information about the DataFrame,
+       including mean, standard deviation, minimum, maximum, and specified percentiles.
+
+    Example:
+    >>> check_data(df)
+    """
+    print(20 * "-" + "Information".center(20) + 20 * "-")
+    print(dataframe.info())
+    print(20 * "-" + "Data Shape".center(20) + 20 * "-")
+    print(dataframe.shape)
+    print("\n" + 20 * "-" + "The First 5 Data".center(20) + 20 * "-")
+    print(dataframe.head())
+    print("\n" + 20 * "-" + "The Last 5 Data".center(20) + 20 * "-")
+    print(dataframe.tail())
+    print("\n" + 20 * "-" + "Missing Values".center(20) + 20 * "-")
+    print((dataframe.isnull().sum()).sort_values(ascending=False))
+    print("\n" + 40 * "-" + "Describe the Data".center(40) + 40 * "-")
+    print(dataframe.describe([0.01, 0.05, 0.10, 0.50, 0.75, 0.90, 0.95, 0.99]).T)
+
+########################
+# Loading the data
+########################
+
+train = pd.read_csv('PROJELER/Demand_Forecasting/demand_forecasting_dataset/train.csv', parse_dates=['date'])
+test = pd.read_csv('PROJELER/Demand_Forecasting/demand_forecasting_dataset/test.csv', parse_dates=['date'])
+
+sample_sub = pd.read_csv('PROJELER/Demand_Forecasting/demand_forecasting_dataset/sample_submission.csv')
+
+df = pd.concat([train, test], sort=False)
+
+#####################################################
+# EDA
+#####################################################
+
+df["date"].min(), df["date"].max()
+
+check_data(df)
+
+df[["store"]].nunique()
+
+df[["item"]].nunique()
+
+df.groupby(["store"])["item"].nunique()
+
+df.groupby(["store", "item"]).agg({"sales": ["sum"]})
+
+df.groupby(["store", "item"]).agg({"sales": ["sum", "mean", "median", "std"]})
+
+df.head()
+
+
+#####################################################
+# FEATURE ENGINEERING
+#####################################################
+
+df.head()
+
+def create_date_features(df):
+    df['month'] = df.date.dt.month
+    df['day_of_month'] = df.date.dt.day
+    df['day_of_year'] = df.date.dt.dayofyear
+    df['week_of_year'] = df.date.dt.isocalendar().week
+    df['day_of_week'] = df.date.dt.dayofweek
+    df['year'] = df.date.dt.year
+    df["is_wknd"] = df.date.dt.weekday // 4
+    df['is_month_start'] = df.date.dt.is_month_start.astype(int)
+    df['is_month_end'] = df.date.dt.is_month_end.astype(int)
+    return df
+
+df = create_date_features(df)
+
+df.groupby(["store", "item", "month"]).agg({"sales": ["sum", "mean", "median", "std"]})
+
+
+########################
+# Random Noise
+########################
+
+def random_noise(dataframe):
+    return np.random.normal(scale=1.6, size=(len(dataframe),))
+
+
+########################
+# Lag/Shifted Features
+########################
+
+df.sort_values(by=['store', 'item', 'date'], axis=0, inplace=True)
+
+pd.DataFrame({"sales": df["sales"].values[0:10],
+              "lag1": df["sales"].shift(1).values[0:10],
+              "lag2": df["sales"].shift(2).values[0:10],
+              "lag3": df["sales"].shift(3).values[0:10],
+              "lag4": df["sales"].shift(4).values[0:10]})
+
+df.groupby(["store", "item"])['sales'].head()
+
+df.groupby(["store", "item"])['sales'].transform(lambda x: x.shift(1))
+
+def lag_features(dataframe, lags):
+    for lag in lags:
+        dataframe['sales_lag_' + str(lag)] = dataframe.groupby(["store", "item"])['sales'].transform(
+            lambda x: x.shift(lag)) + random_noise(dataframe)
+    return dataframe
+
+df = lag_features(df, [91, 98, 105, 112, 119, 126, 182, 364, 546, 728])
+
+check_data(df)
+
+########################
+# Rolling Mean Features
+########################
+
+pd.DataFrame({"sales": df["sales"].values[0:10],
+              "roll2": df["sales"].shift(1).rolling(window=2).mean().values[0:10],
+              "roll3": df["sales"].shift(1).rolling(window=3).mean().values[0:10],
+              "roll5": df["sales"].shift(1).rolling(window=5).mean().values[0:10]})
+
+
+def roll_mean_features(dataframe, windows):
+    for window in windows:
+        dataframe['sales_roll_mean_' + str(window)] = dataframe.groupby(["store", "item"])['sales']. \
+                                                          transform(
+            lambda x: x.shift(1).rolling(window=window, min_periods=10, win_type="triang").mean()) + random_noise(
+            dataframe)
+    return dataframe
+
+
+df = roll_mean_features(df, [365, 546])
+
+########################
+# Exponentially Weighted Mean Features
+########################
+
+pd.DataFrame({"sales": df["sales"].values[0:10],
+              "roll2": df["sales"].shift(1).rolling(window=2).mean().values[0:10],
+              "ewm099": df["sales"].shift(1).ewm(alpha=0.99).mean().values[0:10],
+              "ewm095": df["sales"].shift(1).ewm(alpha=0.95).mean().values[0:10],
+              "ewm07": df["sales"].shift(1).ewm(alpha=0.7).mean().values[0:10],
+              "ewm02": df["sales"].shift(1).ewm(alpha=0.1).mean().values[0:10]})
+
+def ewm_features(dataframe, alphas, lags):
+    for alpha in alphas:
+        for lag in lags:
+            dataframe['sales_ewm_alpha_' + str(alpha).replace(".", "") + "_lag_" + str(lag)] = \
+                dataframe.groupby(["store", "item"])['sales'].transform(lambda x: x.shift(lag).ewm(alpha=alpha).mean())
+    return dataframe
+
+alphas = [0.95, 0.9, 0.8, 0.7, 0.5]
+lags = [91, 98, 105, 112, 180, 270, 365, 546, 728]
+
+df = ewm_features(df, alphas, lags)
+check_data(df)
+
+########################
+# One-Hot Encoding
+########################
+
+df = pd.get_dummies(df, columns=['store', 'item', 'day_of_week', 'month'])
+
+
+check_data(df)
+
+
+########################
+# Converting sales to log(1+sales)
+########################
+
+df['sales'] = np.log1p(df["sales"].values)
+
+check_data(df)
+
+#####################################################
+# Model
+#####################################################
+
+########################
+# Custom Cost Function
+########################
+
+# MAE, MSE, RMSE, SSE
+
+# MAE: mean absolute error
+# MAPE: mean absolute percentage error
+# SMAPE: Symmetric mean absolute percentage error (adjusted MAPE)
+
+def smape(preds, target):
+    n = len(preds)
+    masked_arr = ~((preds == 0) & (target == 0))
+    preds, target = preds[masked_arr], target[masked_arr]
+    num = np.abs(preds - target)
+    denom = np.abs(preds) + np.abs(target)
+    smape_val = (200 * np.sum(num / denom)) / n
+    return smape_val
+
+
+def lgbm_smape(preds, train_data):
+    labels = train_data.get_label()
+    smape_val = smape(np.expm1(preds), np.expm1(labels))
+    return 'SMAPE', smape_val, False
+
+
+
+
+########################
+# Time-Based Validation Sets
+########################
+
+train
+test
+
+# train set until the beginning of 2017 (until the end of 2016).
+train = df.loc[(df["date"] < "2017-01-01"), :]
+
+# the first 3rd validation set of 2017.
+val = df.loc[(df["date"] >= "2017-01-01") & (df["date"] < "2017-04-01"), :]
+
+cols = [col for col in train.columns if col not in ['date', 'id', "sales", "year"]]
+
+Y_train = train['sales']
+X_train = train[cols]
+
+Y_val = val['sales']
+X_val = val[cols]
+
+Y_train.shape, X_train.shape, Y_val.shape, X_val.shape
+
+########################
+# Time Series Model with LightGBM
+########################
+
+# !pip install lightgbm
+# conda install lightgbm
+
+
+# LightGBM parameters
+lgb_params = {'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'num_boost_round': 1000,
+              'early_stopping_rounds': 200,
+              'nthread': -1}
+
+# metric mae: l1, absolute loss, mean_absolute_error, regression_l1
+# mse: l2, square loss, mean_squared_error, mse, regression_l2, regression
+# rmse, root square loss, root_mean_squared_error, l2_root
+# mape, MAPE loss, mean_absolute_percentage_error
+
+# num_leaves: the maximum number of leaves on a tree
+# learning_rate: shrinkage_rate, eta
+# feature_fraction: the random subspace property of rf. the number of variables that will be randomly considered in each iteration.
+# max_depth: maximum depth
+# num_boost_round: n_estimators, number of boosting iterations. We need to do at least about 10000-15000.
+
+# early_stopping_rounds: if the metric in the validation set does not progress at a certain early_stopping_rounds, that is
+# if the error is not falling, stop modeling.
+# it both shortens the train time and prevents overfit.
+# nthread: num_thread, nthread, nthreads, n_jobs
+
+lgbtrain = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+
+lgbval = lgb.Dataset(data=X_val, label=Y_val, reference=lgbtrain, feature_name=cols)
+
+model = lgb.train(lgb_params, lgbtrain,
+                  valid_sets=[lgbtrain, lgbval],
+                  num_boost_round=lgb_params['num_boost_round'],
+                  early_stopping_rounds=lgb_params['early_stopping_rounds'],
+                  feval=lgbm_smape,
+                  verbose_eval=100)
+
+y_pred_val = model.predict(X_val, num_iteration=model.best_iteration)
+
+smape(np.expm1(y_pred_val), np.expm1(Y_val))
+
+
+########################
+# Feature Importance
+########################
+
+def plot_lgb_importances(model, plot=False, num=10):
+    gain = model.feature_importance('gain')
+    feat_imp = pd.DataFrame({'feature': model.feature_name(),
+                             'split': model.feature_importance('split'),
+                             'gain': 100 * gain / gain.sum()}).sort_values('gain', ascending=False)
+    if plot:
+        plt.figure(figsize=(10, 10))
+        sns.set(font_scale=1)
+        sns.barplot(x="gain", y="feature", data=feat_imp[0:25])
+        plt.title('feature')
+        plt.tight_layout()
+        plt.show()
+    else:
+        print(feat_imp.head(num))
+    return feat_imp
+
+plot_lgb_importances(model, num=200)
+
+plot_lgb_importances(model, num=30, plot=True)
+
+
+feat_imp = plot_lgb_importances(model, num=200)
+
+importance_zero = feat_imp[feat_imp["gain"] == 0]["feature"].values
+
+imp_feats = [col for col in cols if col not in importance_zero]
+len(imp_feats)
+
+
+########################
+# Final Model
+########################
+
+train = df.loc[~df.sales.isna()]
+Y_train = train['sales']
+X_train = train[cols]
+
+
+test = df.loc[df.sales.isna()]
+X_test = test[cols]
+
+lgb_params = {'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'nthread': -1,
+              "num_boost_round": model.best_iteration}
+
+lgbtrain_all = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+
+final_model = lgb.train(lgb_params, lgbtrain_all, num_boost_round=model.best_iteration)
+
+
+
+test_preds = final_model.predict(X_test, num_iteration=model.best_iteration)
+
+########################
+# Submission File
+########################
+
+test.head()
+
+submission_df = test.loc[:, ["id", "sales"]]
+submission_df['sales'] = np.expm1(test_preds)
+
+submission_df['id'] = submission_df.id.astype(int)
+
+submission_df.to_csv("submission_demand.csv", index=False)
